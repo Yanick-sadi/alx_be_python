d33e536f516ba26d606df841f5353d6d0fb75066
Yanick <nckmuhire@gmail.com>
2025-07-21 00:46:52 +0200

    add file

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,5 @@
 number = int(input("Enter a number to see its multiplication table:"))
 
-for x in range(1, 10):
+for x in range(1, 11):
     product = x * number
     print(f"{x} * {number} = {product}")
