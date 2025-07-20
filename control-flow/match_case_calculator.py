76a29c904a4beac43a0793cb0aa51baac466303e
Yanick <nckmuhire@gmail.com>
2025-07-21 00:24:05 +0200

    add file

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,9 +1,13 @@
 num1 = int(input("Enter the first number:"))
 num2 = int(input("Enter the second number:"))
-operation = input("Choose the operation (+, -, *, /):")
+operation = input("Choose the operation (+, -, *, /):").lower()
 
 match operation:
-    case addition:
-        sum = num1 + num2
-        print(sum)
-    case subtraction
+    case "+":
+        print(f"the result is {num1 + num2}")
+    case "-":
+        print(f"the result is {num1 - num2}")
+    case "*":
+        print(f"the result is {num1 * num2}")
+    case "/":
+        print(f"the result is {num1 / num2}")
