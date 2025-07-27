c2f4757838a3b2af58dee393c60a8d2827acfc96
Yanick <nckmuhire@gmail.com>
2025-07-27 22:19:15 +0200

    add file

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,33 @@
+# def sum(a, b):
+#     return a + b
+
+
+# def subtract(a, b):
+#     return a - b
+
+
+# def multiply(a, b):
+#     return a * b
+
+
+# def quotient(a, b):
+
+#     if b == 0:
+
+#         return "error: can not divide by zero!"
+
+#     return a / b
+def perform_operation(num1, num2, operation):
+        if operation == "add":
+            return num1 + num2
+        if operation == "subtract":
+            return  num1 - num2
+        if operation == "multiply":
+            return num1 * num2
+        if operation == "divide":
+            return num1 / num2
+            if num2 == 0:
+                return "error: can not divide by zero!"
+
+        
+
